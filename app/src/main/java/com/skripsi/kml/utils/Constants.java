7f3979cf1d980e52c6eedae8edfe22db336fec06
Yuda Kurniawan <55118789+YudhaK@users.noreply.github.com>
2019-09-11 07:43:52 +0700

    Update Constants.java

--- app/src/main/java/com/skripsi/kml/utils/Constants.java
+++ app/src/main/java/com/skripsi/kml/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.skripsi.kml.utils;
 
 public class Constants {
-    public static final String URL = "http://riffaldy.com/region_db/API/index.php";
-    public static final String LAPORAN = "http://riffaldy.com/region_db/API/laporan.php";
+    public static final String URL = "your hosting address/your DB/API/index.php";
+    public static final String LAPORAN = "your hosting address/your DB/API/laporan.php";
     public static final String TAG_SUCCESS = "1";
 }
